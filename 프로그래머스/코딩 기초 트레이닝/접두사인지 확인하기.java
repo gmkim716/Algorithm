e97afd3ab4d9348db4cecb63359888b831841088
Gyeongmin Kim <gmkim716@gmail.com>
2024-10-07 19:07:05 +0900

    update

--- 프로그래머스/코딩 기초 트레이닝/접두사인지 확인하기.java
+++ 프로그래머스/코딩 기초 트레이닝/접두사인지 확인하기.java
@@ -1,19 +1,5 @@
-public class Solution {
+class Solution {
   public int solution(String my_string, String is_prefix) {
-      boolean result = true; 
-      
-      if (is_prefix.length() > my_string.length()) {
-          result = false; 
-      } else {
-          for (int i=0; i<is_prefix.length(); i++) {
-              if (is_prefix.charAt(i) != my_string.charAt(i)) {
-                  result = false; 
-              } 
-          }
-      }
-          
-      return result ? 1 : 0; 
+      return my_string.startsWith(is_prefix) ? 1 : 0; 
   }
-} {
-  
 }
