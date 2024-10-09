26ef9e3d6c0493f3a53edc8ab7cdf7d73a223c9c
Gyeongmin Kim <gmkim716@gmail.com>
2024-10-09 22:58:13 +0900

    배열만들기 1

---
+++ 프로그래머스/코딩 기초 트레이닝/배열만들기 1.java
@@ -0,0 +1,23 @@
+public import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int n, int k) {
+        ArrayList<Integer> multiples = new ArrayList<>();
+
+        // k의 배수를 찾기 위해 반복문 사용
+        for (int i = k; i <= n; i += k) {
+            multiples.add(i);
+        }
+
+        // ArrayList를 int 배열로 변환
+        int[] result = new int[multiples.size()];
+        for (int i = 0; i < multiples.size(); i++) {
+            result[i] = multiples.get(i);
+        }
+
+        return result;
+    }
+}
+ {
+  
+}
