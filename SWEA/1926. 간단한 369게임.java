a55e2d76d464bfe95c66851f7a1492c24a42d590
Gyeongmin Kim <gmkim716@gmail.com>
2024-10-16 22:17:18 +0900

    간단한 369 게임

--- SWEA/1926. 간단한 369게임.java
+++ SWEA/1926. 간단한 369게임.java
@@ -1,5 +1,3 @@
-package org.example;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
@@ -26,7 +24,7 @@ public class Solution {
                 }
                 System.out.print(" ");
             }
-            }
+        }
 
         br.close();
     }
